e0ad1a697e354b277a726816212f67d518541ec0
unknown <ricardo.valim@gmail.com>
2019-04-10 13:36:48 +0100

    removing desnecessary code

--- src/actions/categories.js
+++ src/actions/categories.js
@@ -1,14 +1,4 @@
-import * as api from '../utils/api'
-
-export const CATEGORY_GET = 'CATEGORY_GET'
 export const CATEGORY_SET = 'CATEGORY_SET'
-
-function _getCategories(categories) {
-    return {
-      type: CATEGORY_GET,
-      categories,
-    }
-  }
 
 export function setCategories(categories){
   return {
